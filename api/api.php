cc1dda86fd506a8b079b894d2977408620cb42a8
Aaron Eiche <aaron@aaroneiche.com>
2016-10-15 13:34:34 -0700

    Adds users REST query.
    Signed-off-by: Aaron Eiche <aaron@aaroneiche.com>

--- api/api.php
+++ api/api.php
@@ -12,7 +12,7 @@ $app->get('/update', function ($request, $response, $args) {
     return $response->write("Run Update method");
 });
 
-$app->get('/user/{userid}/items', function($req, $res, $args) {
+$app->get('/users/{userid}/items', function($req, $res, $args) {
   $listItems = Model::factory('Items')->where('userid',$args['userid'])->findArray();
   if($listItems !== false){
     $res->withJSON($listItems);
@@ -20,6 +20,17 @@ $app->get('/user/{userid}/items', function($req, $res, $args) {
     $res->withJSON(array("message"=>"failure"));
   }
 });
+
+$app->get('/users', function($req, $res, $args) {
+  $users = Model::factory('Users')->findArray();
+
+  if($users !== false){
+    $res->withJSON($users);
+  }else{
+    $res->withJSON(array("message"=>"failure"));
+  }
+});
+
 
 // Run app
 $app->run();
